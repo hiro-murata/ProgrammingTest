958935f8fa94ac00b6ce5a0db73edb7f80df6b5c
hiro-murata <38581138+hiro-murata@users.noreply.github.com>
2018-09-18 00:50:58 +0000

    Update GetSheetsData.py

--- GetSheetsData.py
+++ GetSheetsData.py
@@ -1,5 +1,5 @@
 # ここにAPI_KEYを設定してください。
-API_KEY = 'AIzaSyDwxpicDSa3GBcLJmgE1yxdtjYpIJFogcA'
+API_KEY = ''
 SPREADSHEET_ID = '11BCnspCt2Mut3nhc4WMY6CYTd0zF9C3eCzsk1AEpKLM'
 RANGE = 'sales!A1:E6'
 DATA = 'https://sheets.googleapis.com/v4/spreadsheets/' + SPREADSHEET_ID + '/values/' + RANGE + '?key=' + API_KEY
